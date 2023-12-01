ee33b22083dae6ff43f58e2491c2f0b95ae49d8a
freetimezp <kraftukrnet@ukr.net>
2023-12-01 19:36:28 +0200

    06. js-portfolio-34-blogs. Main styles. Post detail component.

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,64 +1,28 @@
 import React, { useState } from 'react';
 
-import Thumbnail1 from '../images/blog1.jpg';
-import Thumbnail2 from '../images/blog2.jpg';
-import Thumbnail3 from '../images/blog3.jpg';
-import Thumbnail4 from '../images/blog4.jpg';
-
 import PostItem from './PostItem';
 
-const DUMMY_POSTS = [
-    {
-        id: 1,
-        thumbnail: Thumbnail1,
-        category: 'education',
-        title: 'This is the title 1. This is the title 1. This is the title 1. This is the title 1. This is the title 1. This is the title 1',
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsam porro assumenda placeat illum, quisquam eligendi nisi. Praesentium, quaerat aliquid",
-        authorID: 3,
-    },
-    {
-        id: 2,
-        thumbnail: Thumbnail2,
-        category: 'science',
-        title: 'This is the title 2',
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsam porro assumenda placeat illum, quisquam eligendi nisi. Praesentium, quaerat aliquid",
-        authorID: 1,
-    },
-    {
-        id: 3,
-        thumbnail: Thumbnail3,
-        category: 'weather',
-        title: 'This is the title 3',
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsam porro assumenda placeat illum, quisquam eligendi nisi. Praesentium, quaerat aliquid",
-        authorID: 13,
-    },
-    {
-        id: 4,
-        thumbnail: Thumbnail4,
-        category: 'farming',
-        title: 'This is the title 4',
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsam porro assumenda placeat illum, quisquam eligendi nisi. Praesentium, quaerat aliquid",
-        authorID: 11,
-    },
-];
+import { DUMMY_POSTS } from '../data';
 
 const Posts = () => {
     const [posts, setPosts] = useState(DUMMY_POSTS);
 
     return (
         <section className='posts'>
-            <div className="container posts__container">
-                {posts.map(({ id, thumbnail, title, desc, category, authorID }) => (
-                    <PostItem key={id}
-                        postId={id}
-                        thumbnail={thumbnail}
-                        title={title}
-                        desc={desc}
-                        category={category}
-                        authorID={authorID}
-                    />
-                ))}
-            </div>
+            {posts.length > 0 ? (
+                <div className="container posts__container">
+                    {posts.map(({ id, thumbnail, title, desc, category, authorID }) => (
+                        <PostItem key={id}
+                            postId={id}
+                            thumbnail={thumbnail}
+                            title={title}
+                            desc={desc}
+                            category={category}
+                            authorID={authorID}
+                        />
+                    ))}
+                </div>
+            ) : <h2 className='center'>No posts found..</h2>}
         </section>
     );
 };
--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
 
+import PostAuthor from '../components/PostAuthor';
+import { Link } from 'react-router-dom';
+
+import Thumbnail from '../images/blog22.jpg';
+
 const PostDetail = () => {
     return (
-        <div>
-            PostDetail
-        </div>
+        <section className='post-detail'>
+            <div className="container post-detail__container">
+                <div className="post-detail__header">
+                    <PostAuthor />
+                    <div className="post-detail__buttons">
+                        <Link to={`/posts/werwer/edit`} className='btn sm primary'>
+                            Edit
+                        </Link>
+                        <Link to={`/posts/werwer/delete`} className='btn sm danger'>
+                            Delete
+                        </Link>
+                    </div>
+                </div>
+
+                <h1>This is the post title.</h1>
+
+                <div className="post-detail__thumbnail">
+                    <img src={Thumbnail} alt="post" />
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+                    sint sequi quasi laborum laudantium numquam, temporibus delectus
+                    soluta? Rem sed provident dolores voluptatum delectus, iste consequatur
+                    aperiam similique pariatur, placeat vitae, illo expedita nemo! Incidunt
+                    maiores exercitationem ducimus facilis velit.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores soluta
+                    ut incidunt dignissimos itaque. Repudiandae, eveniet? Velit, eligendi
+                    excepturi. Quod ex deserunt doloribus reprehenderit dicta obcaecati
+                    eligendi voluptatum repudiandae accusamus nihil, maxime aliquam,
+                    necessitatibus excepturi, quos temporibus laborum neque laboriosam natus!
+                    In debitis doloribus eum eaque tempora rem, maxime dignissimos quos labore
+                    sequi est tempore.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+                    magni possimus, hic architecto doloremque exercitationem.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus
+                    eaque nisi eum dolores! Impedit tenetur praesentium repellat culpa ad dolores
+                    reprehenderit ipsam itaque! Quae exercitationem numquam, doloribus autem
+                    similique perferendis dolor officia iusto quis sequi unde ipsa aperiam?
+                    Exercitationem error deserunt et eius dolore quos tempora cumque dicta aliquam
+                    autem magnam cupiditate repudiandae molestias placeat nemo sit corporis saepe
+                    libero praesentium sed commodi, natus dolorum? Enim qui, accusamus cupiditate
+                    nam unde nulla iure quam porro tenetur similique ipsum quaerat corporis magni
+                    impedit magnam exercitationem. In pariatur consequuntur saepe, nemo ullam
+                    quaerat sint mollitia, hic voluptatibus nam quod? Dicta esse earum itaque
+                    corrupti distinctio! Et illo nostrum a voluptate, tempore enim saepe hic rerum
+                    magnam vel sed ea placeat consequatur quia maxime temporibus itaque cupiditate
+                    assumenda! Cumque, necessitatibus? Ipsa recusandae aspernatur quod in.
+                    Asperiores error ipsa facere repellat laborum tenetur voluptatibus eum cupiditate
+                    quam, earum inventore sequi? Reprehenderit quas provident id a animi voluptatum
+                    excepturi alias, at quod illum. Earum nemo autem voluptatum delectus totam!
+                    Facere doloremque a, sunt fugit sequi odio qui veritatis vero laboriosam quas
+                    quidem iste nesciunt eligendi assumenda maxime consequuntur, aspernatur vel
+                    itaque. Sint, impedit. Voluptatum atque itaque totam voluptatem quisquam iste,
+                    sint ut officiis suscipit.
+                </p>
+            </div>
+        </section>
     );
 };
 
