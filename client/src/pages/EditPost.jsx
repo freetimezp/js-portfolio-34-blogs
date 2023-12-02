34a49399be898c492696e33798aaf20298024c2e
freetimezp <kraftukrnet@ukr.net>
2023-12-02 19:42:27 +0200

    11. js-portfolio-34-blogs. Create, Edit post component.

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const EditPost = () => {
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('Uncategorized');
+    const [desc, setDesc] = useState('');
+    const [thumbnail, setThumbnail] = useState('');
+
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockqoute'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['clean']
+        ],
+    };
+
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockqoute',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+    ];
+
+    const POST_CATEGORIES = [
+        'Agriculture',
+        'Business',
+        'Education',
+        'Entertainment',
+        'Art',
+        'Investment',
+        'Uncategorized',
+        'Weather'
+    ];
+
     return (
-        <div>
-            EditPost
-        </div>
+        <section className='edit-post'>
+            <div className="container">
+                <h2>Edit Post</h2>
+                <p className="form__error-message">
+                    This is an error message..
+                </p>
+
+                <form className="form edit-post__form">
+                    <input type="text" placeholder='Title' value={title}
+                        onChange={e => setTitle(e.target.value)} />
+                    <select name="category" value={category} onChange={e => setCategory(e.target.value)}>
+                        {POST_CATEGORIES.map((cat, i) => (
+                            <option key={i}>{cat}</option>
+                        ))}
+                    </select>
+
+                    <ReactQuill modules={modules} formats={formats} value={desc}
+                        onChange={setDesc} />
+
+                    <input type="file" onChange={e => setThumbnail(e.target.files[0])} accept="png, jpg, jpeg" />
+                    <button type="submit" className='btn primary'>
+                        Update
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 };
 
