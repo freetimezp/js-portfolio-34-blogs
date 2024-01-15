3166f459ce2934cab1d8ea9e566a5ab34a9a729e
freetimezp <kraftukrnet@ukr.net>
2024-01-15 16:23:20 +0200

    22. js-portfolio-34-blogs. PostController. Create new Post. Mongo db.

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,8 +1,54 @@
+const Post = require("../models/postModel");
+const User = require("../models/userModel");
+const HttpError = require("../models/errorModel");
+
+const path = require("path");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+
 
 // CREATE POST
 // POST : api/posts/create
 const createPost = async (req, res, next) => {
-    res.json("Create post");
+    try {
+        let { title, category, description } = req.body;
+        if (!title || !category || !description || !req.files) {
+            return next(new HttpError("Fill all fields for create post!", 422));
+        }
+        const { thumbnail } = req.files;
+        //check img size
+        if (thumbnail.size > 2000000) {
+            return next(new HttpError("Size of img is to big. Try upload img with size less than 2mb!", 422));
+        }
+        let filename = thumbnail.name;
+        let splittedFilename = filename.split('.');
+        let newFilename = splittedFilename[0] + uuid() + "." + splittedFilename[splittedFilename.length - 1];
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFilename), async (err) => {
+            if (err) {
+                return next(new HttpError(err));
+            } else {
+                //create new post
+                const newPost = await Post.create({
+                    title, category, description,
+                    thumbnail: newFilename,
+                    creator: req.user.id
+                });
+                if (!newPost) {
+                    return next(new HttpError("Failed to create post!", 422));
+                }
+
+                //find user and increase post count by 1
+                const currentUser = await User.findById(req.user.id);
+                const userPostCount = currentUser.posts + 1;
+                await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
+
+                res.status(201).json(newPost);
+            }
+        });
+
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 // GET POSTS
