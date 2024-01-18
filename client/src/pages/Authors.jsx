ba429b449f1395f898002f2fc325475d6c80d3e5
freetimezp <kraftukrnet@ukr.net>
2024-01-18 17:47:12 +0200

    37. js-portfolio-34-blogs. Show all users/authors from db.

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import Avatar1 from '../images/avatar1.jpg';
-import Avatar2 from '../images/avatar2.jpg';
-import Avatar3 from '../images/avatar3.jpg';
-import Avatar4 from '../images/avatar4.jpg';
-import Avatar5 from '../images/avatar5.jpg';
 import { useCheckUserLogged } from '../context/userContext';
-
-const authorData = [
-    { id: 1, avatar: Avatar1, name: 'Ernest Achiever', posts: 3 },
-    { id: 2, avatar: Avatar2, name: 'Jane Doe', posts: 5 },
-    { id: 3, avatar: Avatar3, name: 'Dramanu Mahama', posts: 0 },
-    { id: 4, avatar: Avatar4, name: 'Nana Addo', posts: 2 },
-    { id: 5, avatar: Avatar5, name: 'Hajiay Bintu', posts: 1 },
-];
+import Loader from '../components/Loader';
+import axios from 'axios';
 
 const Authors = () => {
-    const [authors, setAuthors] = useState(authorData);
+    const [authors, setAuthors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useCheckUserLogged();
+
+    useEffect(() => {
+        const getAuthors = async () => {
+            setIsLoading(true);
+
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/users`);
+                setAuthors(response?.data);
+            } catch (err) {
+                console.log(err);
+            }
+
+            setIsLoading(false);
+        }
+
+        getAuthors();
+    }, []);
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return (
         <section className='authors'>
             {authors.length > 0 ? (
                 <div className="container authors__container">
-                    {authors.map(({ id, name, avatar, posts }) => (
+                    {authors.map(({ _id: id, name, avatar, posts }) => (
                         <Link to={`/posts/users/${id}`} key={id} className='author'>
                             <div className="author__avatar">
-                                <img src={avatar} alt={`avatar of name: ${name}`} />
+                                <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${avatar}`}
+                                    alt={`avatar of name: ${name}`} />
                             </div>
                             <div className="author__info">
                                 <h4>{name}</h4>
